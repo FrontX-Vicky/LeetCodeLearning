673ca7a3d9c221722d6a8ee90f68f9b7080615e6
vicky <vickymore199888@gmail.com>
2026-07-02 13:45:23 +0530

    feat: add large test case visualization for minCostClimbingStairs

--- 25_dp_introduction/test_cases.py
+++ 25_dp_introduction/test_cases.py
@@ -9,17 +9,20 @@ class TestDPIntroduction(unittest.TestCase):
         self.assertEqual(fib(4), 3)
         self.assertEqual(fib(0), 0)
         self.assertEqual(fib(10), 55)
+        self.assertEqual(fib(30), 832040)
 
     def test_climbStairs(self):
         self.assertEqual(climbStairs(2), 2)
         self.assertEqual(climbStairs(3), 3)
         self.assertEqual(climbStairs(5), 8)
         self.assertEqual(climbStairs(1), 1)
+        self.assertEqual(climbStairs(35), 14930352)
 
     def test_minCostClimbingStairs(self):
         self.assertEqual(minCostClimbingStairs([10, 15, 20]), 15)
         self.assertEqual(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
         self.assertEqual(minCostClimbingStairs([0, 0, 0, 0]), 0)
+        self.assertEqual(minCostClimbingStairs([1, 100] * 15), 15)
 
 if __name__ == '__main__':
     unittest.main()
