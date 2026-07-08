adc6d73b95e3e99fdbbf6d0d40ae915844130a07
vicky <vickymore199888@gmail.com>
2026-07-08 20:54:31 +0530

    feat: implement countSubstrings and optimize longestPalindrome to use index pointers

--- 31_dp_strings/main.py
+++ 31_dp_strings/main.py
@@ -4,13 +4,14 @@ def longestPalindrome(s: str) -> str:
     """
     res = ""
     res_len = 0
+    res_start = 0
 
     for i in range(len(s)):
 
         l, r = i, i
         while l >= 0 and r < len(s) and s[l] == s[r]:
             if (r - l + 1) > res_len:
-                res = s[l:r+1]
+                res_start = l
                 res_len = r - l + 1
             l -= 1
             r += 1
@@ -18,18 +19,35 @@ def longestPalindrome(s: str) -> str:
         l, r = i, i + 1
         while l >= 0 and r < len(s) and s[l] == s[r]:
             if (r - l + 1) > res_len:
-                res = s[l:r+1]
+                res_start = l
                 res_len = r - l + 1
             l -= 1
             r += 1
     
-    return res
+    return s[res_start : res_start + res_len]
 
 def countSubstrings(s: str) -> int:
     """
     Given a string s, return the number of palindromic substrings in it.
     """
-    pass
+    res = 0
+
+    for i in range(len(s)):
+        # Odd length
+        l, r = i, i
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        
+        # Even length
+        l, r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+
+    return res 
 
 def main():
     print("Welcome to Day 31: DP - String Problems (Palindromes)!")
