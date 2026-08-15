7a8d1d76ccc07a7a90452005da94a7bbf5c96323
vicky <vickymore199888@gmail.com>
2026-08-16 01:43:59 +0530

    feat: add interactive visualizations for Trapping Rain Water and Median from Data Stream problems

--- 39_hard_problems_marathon/main.py
+++ 39_hard_problems_marathon/main.py
@@ -1,3 +1,5 @@
+import heapq
+import heapq
 from typing import List
 
 class HardMarathon:
@@ -6,26 +8,57 @@ class HardMarathon:
         Given n non-negative integers representing an elevation map where the width of each bar is 1, 
         compute how much water it can trap after raining.
         """
-        pass
+        if not height:
+            return 0
+        
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l+=1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
 
 class MedianFinder:
     def __init__(self):
         """
         initialize your data structure here.
         """
-        pass
+        self.small = []
+        self.large = []
 
     def addNum(self, num: int) -> None:
         """
         Adds a number into the data structure.
         """
-        pass
+        heapq.heappush(self.small, -num)
+
+        if self.small and self.large and (-self.small[0] > self.large[0]):
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        if len(self.small) > len(self.large) + 1:
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -val)
 
     def findMedian(self) -> float:
         """
         Returns the median of current data stream.
         """
-        pass
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2.0
 
 def main():
     print("Welcome to Day 39: Hard Problems Marathon!")
